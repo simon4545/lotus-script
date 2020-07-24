7b33a562439e5153ce8a37d75234a6fc59ae65c5
aimkiray <root@meowwoo.com>
2020-07-24 16:14:37 +0800

    feat: pledge by worker list

--- utils/pledge.py
+++ utils/pledge.py
@@ -9,7 +9,8 @@ import logging
 LOG_FORMAT = "%(asctime)s %(levelname)s %(message)s"
 DATE_FORMAT = "%m-%d-%Y %H:%M:%S"
 
-logging.basicConfig(filename='pledge.log', level=logging.NOTSET, format=LOG_FORMAT, datefmt=DATE_FORMAT)
+logging.basicConfig(filename='pledge.log', level=logging.INFO,
+                    format=LOG_FORMAT, datefmt=DATE_FORMAT)
 
 os.environ["LOTUS_PATH"] = "/data2/lotus-calibration/daemon"
 os.environ["LOTUS_STORAGE_PATH"] = "/data2/lotus-calibration/miner"
@@ -17,10 +18,10 @@ os.environ["WORKER_PATH"] = "/data2/lotus-calibration/worker"
 os.environ["FIL_PROOFS_PARAMETER_CACHE"] = "/data2/proofs/v27"
 
 if __name__ == "__main__":
-    # worker_list = sp.Popen(["./lotus-miner", "workers", "list"], stdout=sp.PIPE, stderr=sp.PIPE).stdout
+    worker_list = sp.Popen(["./lotus-miner", "sealing", "workers"], stdout=sp.PIPE, stderr=sp.PIPE).stdout
     # worker_list = sp.run(["./lotus-miner", "workers", "list"], capture_output=True)
-    worker_list = open("./test.log", 'r', encoding='utf-8',
-               errors='ignore')
+    # worker_list = open("./test.log", 'r', encoding='utf-8',
+    #                    errors='ignore')
     # try:
     #     worker_list = sp.run("./lotus-miner workers list", capture_output=True)
     # except:
@@ -31,18 +32,21 @@ if __name__ == "__main__":
 
     worker_text = ""
     for line in worker_list.readlines():
-        worker_text = worker_text + line.strip()
+        worker_text = worker_text + line.decode(encoding='UTF-8').strip()
 
-    worker_info_list = re.finditer(r'Worker ([\w\W]*?) core', worker_text, re.MULTILINE)
+    worker_info_list = re.finditer(
+        r'Worker ([\w\W]*?) core', worker_text, re.MULTILINE)
 
     for info in worker_info_list:
         # Worker 0 is miner, do noting.
         if (info.group(1)[0:1] == "0"):
             continue
 
+        logging.info("Worker {worker_id} has {core_num} core(s) in use.".format(
+            worker_id=info.group(1)[0:1], core_num=info.group(1)[-1]))
+
         # Check CPU usage
         if (info.group(1)[-1] == "0"):
-            logging.info("Make a sector.")
-            # worker_list = sp.Popen(["./lotus-miner", "sectors", "pledge"])
-        else:
-            logging.info("Worker {worker_id} has {core_num} core(s) in use.".format(worker_id = info.group(1)[0:1], core_num = info.group(1)[-1]))
+            logging.info("Add sector x4.")
+            for i in range(3):
+                worker_list = sp.Popen(["./lotus-miner", "sectors", "pledge"])
