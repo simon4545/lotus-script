44ae8361d64553664b65698fb40c604b1f00fcde
aimkiray <root@meowwoo.com>
2020-09-10 14:29:44 +0800

    feat: speed up

--- prepare-system/prepare-bin.sh
+++ prepare-system/prepare-bin.sh
@@ -9,10 +9,11 @@ cur_dir="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
 cur_date="$(date "+%m%d-%H")"
 
 lotus_rep="https://github.com/filecoin-project/lotus.git"
-rep_dir="lotus-${cur_date}"
-branch="ntwk-calibration"
+branch="master"
+commit="HEAD"
+rep_dir="lotus-${branch}"
 
-proxy="http://192.168.0.8:8000"
+proxy="http://127.0.0.1:8000"
 
 _red() {
     printf '\033[1;31;31m%b\033[0m' "$1"
@@ -66,35 +67,47 @@ _error_detect() {
 }
 
 prepare_apt() {
-    # sed -i 's/cn.archive.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
+    sed -i 's/archive.ubuntu.com/mirrors.163.com/g' /etc/apt/sources.list
+    _error_detect "apt-get -y --fix-broken install"
     _error_detect "apt-get -y update"
     _error_detect "apt-get -y install mesa-opencl-icd ocl-icd-opencl-dev gcc git bzr jq pkg-config curl"
     _error_detect "apt-get -y upgrade"
 }
 
 prepare_go() {
-    export http_proxy=${proxy}
-    export https_proxy=${proxy}
-    export GO111MODULE=on
-    export GOPROXY=https://goproxy.cn,direct
-    _error_detect "curl -OL https://golang.org/dl/go1.14.4.linux-amd64.tar.gz"
+    _error_detect "curl -OL https://mirrors.nju.edu.cn/golang/go1.14.4.linux-amd64.tar.gz"
     _error_detect "tar -C /usr/local -xzf go1.14.4.linux-amd64.tar.gz"
     echo "export PATH=$PATH:/usr/local/go/bin" >> $HOME/.profile
     source $HOME/.profile
 }
 
 prepare_rust() {
-    export http_proxy=${proxy}
-    export https_proxy=${proxy}
+    export RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static
+    export RUSTUP_UPDATE_ROOT=https://mirrors.ustc.edu.cn/rust-static/rustup
     _error_detect "curl -sSf https://sh.rustup.rs | sh -s -- -y"
-    source $HOME/.cargo/env
+    echo "source $HOME/.cargo/env" >> $HOME/.profile
+    source $HOME/.profile
+    cat > $HOME/.cargo/config <<EOF
+    [source.crates-io]
+    registry = "https://github.com/rust-lang/crates.io-index"
+    replace-with = 'ustc'
+    [source.ustc]
+    registry = "git://mirrors.ustc.edu.cn/crates.io-index"
+    EOF
 }
 
-# Make bench from source
-make_bench() {
+# Make bin from source
+make_bin() {
+    source $HOME/.cargo/env
+    export PATH=$PATH:/usr/local/go/bin
+    git config --global http.proxy ${proxy}
+    export GO111MODULE=on
+    export GOPROXY=https://goproxy.cn,direct
     cd ${cur_dir}
-    _error_detect "git clone -b ${branch} ${lotus_rep} ${rep_dir}"
+    _error_detect "rm -rf ${rep_dir}"
+    _error_detect "git clone --depth=1 -b ${branch} ${lotus_rep} ${rep_dir}"
     _error_detect "cd ${rep_dir}"
+    _error_detect "git checkout ${commit}"
     _error_detect "git submodule init"
     _error_detect "git submodule update"
     sed -i 's/"check_cpu_for_feature": null/"check_cpu_for_feature": "sha_ni"/g' extern/filecoin-ffi/rust/rustc-target-features-optimized.json
@@ -103,11 +116,11 @@ make_bench() {
 
 main() {
     _info "Prepare environment..."
-    prepare_apt
-    prepare_go
-    prepare_rust
+    #prepare_apt
+    #prepare_go
+    #prepare_rust
     _info "Make lotus from source..."
-    make_bench
+    make_bin
     _info "Done!"
 }
 
